1a9e4d427562be4a2c711a458ce1d90ba7b4c7e6
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-12-29 19:57:18 +0530

    Added Header File and made all the required pages

---
+++ src/app/(auth)/forgot-password/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ForgotPasswordPage() {
+  return (
+    <div>
+    ForgotPasswordPage
+      
+    </div>
+  )
+}
---
+++ src/app/blog/[slug]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SinglePostPage() {
+  return (
+    <div>
+    SinglePostPage
+      
+    </div>
+  )
+}
+
+export default SinglePostPage;
